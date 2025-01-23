bd843e78369ea9cbd21936b28d352242f24f9aaf
A00587995 <A00587995@B7QHF24>
2025-01-23 09:40:39 -0600

    Add project files.

---
+++ lab1b/lab1b.cpp
@@ -0,0 +1,12 @@
+#include"iostream" 
+
+
+using std::cout;
+
+int main()
+{
+	cout << "Hey, my name is Mishell.";		
+	cout << "I'm 19 years old,";
+	cout << " and this is my fourth semester at TJC.";
+	return 0;
+}
